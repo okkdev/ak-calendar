79ec1804b375d0daa5a9b99b08aa5582e1ea48c9
jen.stehlik <jen.stehlik@kallysoft.ch>
2020-01-30 17:40:16 +0100

    get character info

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  async function createCharacterObject() {
+    const res = await fetch(
+      'https://aceship.github.io/AN-EN-Tags/json/gamedata/en/excel/handbook_info_table.json'
+    )
+    let handbook = await res.json()
+    handbook = handbook.handbookDict
+
+    for (let char in handbook) {
+      console.log('-')
+      console.log(handbook[char].charID)
+      console.log(handbook[char].storyTextAudio[0].stories[0].storyText)
+    }
+  }
+
+  createCharacterObject()
+</script>
+
+<main>
+  <div>test</div>
+</main>
